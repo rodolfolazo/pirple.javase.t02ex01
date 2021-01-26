eac1b248f3c73f2e6fe55bcdced43f4871f34539
Rodolfo Lazo <rodolfo.lazo@outlook.com>
2021-01-26 08:02:13 +0000

    Commit inicial

---
+++ src/t02ex01/T02ex01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t02ex01;
+
+/**
+ *
+ * @author rodo
+ */
+public class T02ex01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 8;
+        int y = 5;
+        System.out.println(9/5);
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);       
+        
+    }
+    
+}
